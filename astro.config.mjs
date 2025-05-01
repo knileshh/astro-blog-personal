aef4e64495eb98251fac32cee0985236f4dd1c93
knileshh <neutron_stays@protonmail.com>
2025-05-01 16:16:16 +0530

    Revert "FIX: vercel analytics, vercelServerLess adapter()"
    This reverts commit 8f358e3eca8cf439edb3888e6b30acfa1ed892f2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,54 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts';
-import remarkUnwrapImages from 'remark-unwrap-images';
-import rehypeExternalLinks from 'rehype-external-links';
-import expressiveCode from 'astro-expressive-code';
-import { expressiveCodeOptions } from './src/site.config';
-import icon from 'astro-icon';
-import vercelServerless from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import rehypeExternalLinks from 'rehype-external-links'
+import expressiveCode from 'astro-expressive-code'
+import { expressiveCodeOptions } from './src/site.config'
+import icon from 'astro-icon'
 
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.knileshh.com',
-  integrations: [expressiveCode(expressiveCodeOptions), tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), icon()],
-  markdown: {
-    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-    rehypePlugins: [[rehypeExternalLinks, {
-      target: '_blank',
-      rel: ['nofollow, noopener, noreferrer']
-    }]],
-    remarkRehype: {
-      footnoteLabelProperties: {
-        className: ['']
-      }
-    }
-  },
-  prefetch: true,
-  output: 'server',
-  adapter: vercelServerless({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
-});
+	site: 'https://example.me',
+	integrations: [
+		expressiveCode(expressiveCodeOptions),
+		tailwind({
+			applyBaseStyles: false
+		}),
+		sitemap(),
+		mdx(),
+		icon()
+	],
+	markdown: {
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: '_blank',
+					rel: ['nofollow, noopener, noreferrer']
+				}
+			]
+		],
+		remarkRehype: {
+			footnoteLabelProperties: {
+				className: ['']
+			}
+		}
+	},
+	prefetch: true,
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+		imagesConfig: {
+			sizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+			domains: [],
+			remotePatterns: [],
+			formats: ['image/avif', 'image/webp']
+		}
+	})
+})
